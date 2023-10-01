85161ce8a09ef9494a27f55a5725c0fb8fcdb3f5
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2023-10-01 09:40:47 -0700

    Add itemized cart items and cart total

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,27 +1,76 @@
+import { useEffect, useState } from "react";
 import { useCart } from "../contexts/CartContext";
 import { useProducts } from "../contexts/ProductsContext";
+import { toCurrency } from "../utilities/currency";
+import { Paper } from "@mui/material";
 
 function Cart() {
   // Contexts
   const { productsInCart } = useCart();
   const { products } = useProducts();
 
-  const cartItems = Object.entries(productsInCart);
+  // States
+  const [cartData, setCartData] = useState([]);
 
-  const showCartItems = (cartItem) => {
-    const id = cartItem[0];
-    const quantity = cartItem[1];
-    const product = products.find((element) => element.id == id);
+  useEffect(() => {
+    setCartData(transformCartInfo());
+  }, [productsInCart]);
+
+  const transformCartInfo = () => {
+    const cartItems = Object.entries(productsInCart);
+    const cartData = cartItems.map((cartItem) => {
+      const id = cartItem[0];
+      const quantity = cartItem[1];
+      const product = products.find((element) => element.id == id);
+      return [id, quantity, product];
+    });
+    return cartData;
+  };
+
+  const showCartItems = (item) => {
+    const id = item[0];
+    const quantity = item[1];
+    const product = item[2];
+    const productSubtotal = quantity * product.price;
+
     return (
-      <div key={id}>
+      <div key={id} className="cart-item">
+        <div className="cart-item-img-container">
+          <img className="cart-item-img" src={product.image} />
+        </div>
+        <div className="cart-item-info">
         <h3>
-          [{quantity}] {product.title}: {quantity * product.price}
+          {product.title}
         </h3>
+        <div className="cart-item-data">
+          <p>Qty: {quantity}</p>
+          <h4>{toCurrency(productSubtotal)}</h4>
+        </div>
+        </div>
+
       </div>
     );
   };
 
-  return <div>{cartItems.map(showCartItems)}</div>;
+  return (
+    <div className="cart-container">
+      <Paper className="cart-items-container" elevation={10}>
+        {cartData.map(showCartItems)}
+      </Paper>
+      <Paper className="cart-summary-container" elevation={10}>
+        <h1>
+          Total:{" "}
+          {toCurrency(
+            cartData.reduce(
+              (accumulator, currentValue) =>
+                accumulator + currentValue[1] * currentValue[2].price,
+              0
+            )
+          )}
+        </h1>
+      </Paper>
+    </div>
+  );
 }
 
 export default Cart;
