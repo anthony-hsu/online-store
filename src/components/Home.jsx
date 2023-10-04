7bad5cf02e9de524b5127f6d2ab65bbe79b4e994
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2023-10-04 12:46:55 -0700

    Make sticky header and multiple home pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,21 @@
 function Home() {
   return (
     <div className="home-container">
-      <div className="block-left"></div>
-      <div className="block-right"></div>
+      <div className="home-page home-page-1">
+        <div className="block-left">
+          <h1 className="home-title">Buy whatever you want.</h1>
+          <h1 className="home-title">Whenever you want.</h1>
+        </div>
+        <div className="block-right">
+          <img
+            className="home-img"
+            src="https://elephant.art/wp-content/uploads/2019/11/poop-emoji.jpg"
+          />
+        </div>
+      </div>
+      <div className="home-page home-page-2">
+
+      </div>
     </div>
   );
 }
